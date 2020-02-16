bd9fff959a26bc77e9534c15ff0f5c0fb00f01d7
andamatshikiza <andamatshikiza@gmail.com>
2020-02-16 23:14:40 +0200

    Add project files.

---
+++ FibonacciSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FibonacciSum
+{
+    class Program
+    {
+
+        static int SumOfEvnFibNum(long limit)
+        {
+            if (limit < 2) {
+                return 0;
+            }
+
+            long num1 = 0;
+            long num2 = 2;
+            long sum = num1 + num2;
+
+            while (num2 <= limit)
+            {
+                long num3 = 4 * num2 + num1;
+
+                if (num3 > limit)
+                    break;
+
+                num1 = num2;
+                num2 = num3;
+
+                sum += num2;
+            }
+
+            return(int) sum;
+            
+        }
+        static void Main(string[] args)
+        {
+            long limit = 5000000;
+            Console.WriteLine(SumOfEvnFibNum(limit));
+        }
+    }
+}
